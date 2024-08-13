3c897723099f1c88bd783448a177d8301c0277f0
Jamickin <93577753+Jamickin@users.noreply.github.com>
2024-08-13 08:16:38 +0200

    Fixed Routes

--- src/router/index.js
+++ src/router/index.js
@@ -8,41 +8,6 @@ const router = createRouter({
 			name: "layout",
 			component: () => import("../views/Layout.vue"),
 		},
-
-		{
-			path: "/blackMagic",
-			name: "blackMagic",
-			component: () =>
-				import("../components/blackMagic.vue"),
-		},
-		{
-			path: "/calmingStorm",
-			name: "calmingStorm",
-			component: () =>
-				import("../components/calmingStorm.vue"),
-		},
-		{
-			path: "/aloeVera",
-			name: "aloeVera",
-			component: () => import("../components/aloeVera.vue"),
-		},
-		{
-			path: "/mduziFire",
-			name: "mduziFire",
-			component: () =>
-				import("../components/mduziFire.vue"),
-		},
-		{
-			path: "/pureGoatsMilk",
-			name: "pureGoatsMilk",
-			component: () =>
-				import("../components/pureGoatsMilk.vue"),
-		},
-		{
-			path: "/honeyDew",
-			name: "honeyDew",
-			component: () => import("../components/honeyDew.vue"),
-		},
 	],
 });
 
