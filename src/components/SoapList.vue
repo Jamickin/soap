e76aa03bf5e55b7e314e87817d28667e44523e91
Jamickin <93577753+Jamickin@users.noreply.github.com>
2024-08-23 15:25:32 +0200

    SMall update for banner and duplicate images

--- src/components/SoapList.vue
+++ src/components/SoapList.vue
@@ -1,11 +1,18 @@
 <template>
 	<div class="mt-10 max-w-4xl mx-auto">
+		<!-- Top Banner -->
+		<div
+			v-if="showBanner"
+			class="fixed top-0 left-0 right-0 bg-green-600 text-white text-center py-2 z-50">
+			Item added to cart!
+		</div>
+
 		<!-- Soap List -->
 		<ul class="space-y-4">
 			<li
 				v-for="soap in soaps"
 				:key="soap.id"
-				@click="addToCart(soap)"
+				@click="addToCartWithBanner(soap)"
 				class="flex flex-row-reverse bg-white dark:bg-gray-800 rounded-lg shadow-md p-4 cursor-pointer hover:shadow-lg transition-shadow duration-300">
 				<img
 					:src="soap.image"
@@ -36,7 +43,7 @@
 				@click="toggleCart"
 				:class="{
 					'hover:bg-red-600': showCart,
-					' hover:bg-green-600': !showCart,
+					'hover:bg-green-600': !showCart,
 				}"
 				class="bg-gray-600 text-white font-semibold py-2 px-4 rounded transition duration-300">
 				{{ showCart ? "Hide Cart" : "View Cart" }} ({{
@@ -62,23 +69,23 @@
 				</li>
 			</ul>
 			<div>Total Price: R{{ totalPrice }}</div>
-		</div>
+			<!-- Customer Name and Phone Number Inputs -->
+			<div class="mt-6 space-y-4">
+				<input
+					v-model="customerName"
+					type="text"
+					required
+					class="placeholder:text-gray-500 text-gray-600 w-full border border-gray-300 rounded-md shadow-sm py-2 px-3"
+					placeholder="Your Name" />
 
-		<!-- Customer Name and Phone Number Inputs -->
-		<div class="mt-6 space-y-4">
-			<input
-				v-model="customerName"
-				type="text"
-				class="placeholder:text-gray-500 text-gray-600 w-full border border-gray-300 rounded-md shadow-sm py-2 px-3"
-				placeholder="Your Name" />
-
-			<!-- WhatsApp Button -->
-			<div class="mt-6 text-center">
-				<button
-					@click="prepareWhatsAppMessage"
-					class="bg-green-600 hover:bg-green-800 text-white fo nt-semibold py-2 px-4 rounded transition duration-300">
-					Send Cart Info via WhatsApp
-				</button>
+				<!-- WhatsApp Button -->
+				<div class="mt-6 text-center">
+					<button
+						@click="prepareWhatsAppMessage"
+						class="bg-green-600 hover:bg-green-800 text-white font-semibold py-2 px-4 rounded transition duration-300">
+						Send Cart Info via WhatsApp
+					</button>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -98,9 +105,21 @@
 		prepareWhatsAppMessage,
 		totalPrice,
 	} = useCart();
+
 	const soaps = ref([]);
+	const showBanner = ref(false);
 
 	onMounted(() => {
 		soaps.value = soapData;
 	});
+
+	const addToCartWithBanner = (soap) => {
+		addToCart(soap);
+		showBanner.value = true;
+
+		// Hide the banner after 3 seconds
+		setTimeout(() => {
+			showBanner.value = false;
+		}, 3000);
+	};
 </script>
